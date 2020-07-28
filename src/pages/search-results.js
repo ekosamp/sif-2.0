4436c2463a54322cf454e060bc5225bc981e7973
Danny Di Iorio <dannydiiorio@gmail.com>
2020-07-27 21:55:55 -0700

    modify search form to function on search results page

--- src/pages/search-results.js
+++ src/pages/search-results.js
@@ -8,31 +8,56 @@ import Search from '../components/searchContainer'
 import Heading from '../components/ui/heading'
 import Section, {Row, Col} from '../components/ui/wrapper'
 import CTAArea from '../containers/global/cta-area/section-one'
-import PageHeader from '../components/pageheader'
+import Emoji from '../components/ui/emojis'
+import { ProdQuery } from '../data/hooks/all-product-data'
 
 const SearchResults = ({ pageContext, location, ...restProps }) => {
     const {sectionStyle, headingStyle} = restProps;
+    const allProducts = ProdQuery();
 
-    return (
-        <Layout location={location}>
-            <SEO title="Search Results" />
-            <Header />
-            <main className="site-wrapper-reveal">
-                <Section {...sectionStyle}>
-                    <Row>
-                        <Col xs={12}>
-                            <Heading {...headingStyle}>Search results for: "{location.state.keyword}"</Heading>
-                            <Search
-                                products={location.state.products}
-                                keyword={location.state.keyword} />
-                        </Col>
-                    </Row>
-                </Section>
-                <CTAArea/>
-            </main>
-            <Footer />
-        </Layout>
-    )
+    if (!location.state) {
+        return (
+            <Layout location={location}>
+                <SEO title="Search Results" />
+                <Header isSearchResults={true}/>
+                <main className="site-wrapper-reveal">
+                    <Section {...sectionStyle}>
+                        <Row>
+                            <Col xs={12}>
+                                <Heading {...headingStyle}>Search results for: {<Emoji symbol="👽"/>}</Heading>
+                                <Search
+                                    products={allProducts}
+                                    keyword={``} />
+                            </Col>
+                        </Row>
+                    </Section>
+                    <CTAArea/>
+                </main>
+                <Footer />
+            </Layout>
+        )
+    } else {
+        return (
+            <Layout location={location}>
+                <SEO title="Search Results" />
+                <Header isSearchResults={true}/>
+                <main className="site-wrapper-reveal">
+                    <Section {...sectionStyle}>
+                        <Row>
+                            <Col xs={12}>
+                                <Heading {...headingStyle}>Search results for: "{location.state.keyword}"</Heading>
+                                <Search
+                                    products={location.state.products}
+                                    keyword={location.state.keyword} />
+                            </Col>
+                        </Row>
+                    </Section>
+                    <CTAArea/>
+                </main>
+                <Footer />
+            </Layout>
+        )
+    }
 }
 
 SearchResults.propTypes = {
